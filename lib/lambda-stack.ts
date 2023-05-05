2e05a63c2bdc50a4770b48c0ec3236275cab5303
root <root@ip-10-0-0-7.ec2.internal>
2023-05-05 06:56:59 +0000

    Create ecr

--- lib/lambda-stack.ts
+++ lib/lambda-stack.ts
@@ -2,10 +2,19 @@ import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import { Function, InlineCode, Runtime, Code} from 'aws-cdk-lib/aws-lambda';
 import * as path from 'path';
+const ecr = require('aws-cdk-lib/aws-ecr');
+const ecs = require('@aws-cdk/aws-ecs')
 
 export class MyLambdaStack extends cdk.Stack {
     constructor(scope: Construct, id: string, stageName: string, props?: cdk.StackProps) {
       super(scope, id, props);
+      const repo = ecr.Repository.fromRepositoryArn(
+        this,
+        'Servic1Repo',
+        `arn:aws:ecr:us-west-2:${props?.env?.account}:repository/service1`
+      )
+      const image = ecs.ContainerImage.fromEcrRepository(repo, 'latest')
+      
       new Function(this, 'LambdaFunction', {
         runtime: Runtime.NODEJS_18_X, //using node for this, but can easily use python or other
         handler: 'index.handler',
